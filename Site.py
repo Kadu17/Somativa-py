893d069200df139572af5bd51e0a32a30bb434d0
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-05-11 16:46:24 -0300

    Site.py

--- Site.py
+++ Site.py
@@ -4,33 +4,40 @@ from time import sleep
 
 class Web:
     def __init__(self):
-        self.site = 'https://shopee.com.br/'
+        self.site = ['https://shopee.com.br/Iphone-col.1214627','https://shopee.com.br/Samsung-col.1214628','https://shopee.com.br/Motorola-col.1214629','https://shopee.com.br/Xiaomi-col.1214630']
         self.map = {
             'botao': {
                 'xpath': '/html/body/div[1]/div/div[4]/div/div[3]/button[3]'
             },
             'marca': {
                 'xpath': '/html/body/div[1]/div/div[2]/div/div/div[3]/div[2]/div[4]/div/div/div[2]/div/div[1]/ul/li[5]/div/a[2]',
-                'apple': '/html/body/div[1]/div/div[2]/div/div/div[3]/div[1]/div[2]/div/div[1]/ul/li[1]/div/div/a'
-            }
+                'apple': '/html/body/div[1]/div/div[2]/div/div/div[3]/div[1]/div[2]/div/div[1]/ul/li[1]/div/div/a',
+                'celulares': '/html/body/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]/div[$$]/a/div/div/div[2]/div[1]/div[1]/div',
+                'valores': '/html/body/div[1]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]/div[$$]/a/div/div/div[2]/div[2]/div'
+            },
+
         }
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.abrir()
 
-    def abrir(self):
+        for site in self.site:
+           self.driver = webdriver.Chrome()
+           self.driver.maximize_window()
+           self.abrir(site)
+
+    def abrir(self,site):
+        self.driver.get(site)
+
+
         sleep(2)
-        self.driver.get(self.site)
-        sleep(10)
-        self.driver.find_element(By.XPATH, self.map['botao']['xpath']).click()
-        sleep(5)
-        self.driver.find_element(By.XPATH, self.map['marca']['xpath']).click()
-        sleep(5)
-        self.driver.find_element(By.XPATH, self.map['marca']['apple']).click()
-        sleep(5)
         for i in range(1,11):
-            print(i, end=' ')
+            celular = self.map['marca']['celulares'].replace("$$", f"{i}")
+            item = self.driver.find_element(By.XPATH, celular).text
+            print(item)
 
+        sleep(2)
+        for j in range(1, 11):
+            valores = self.map['marca']['valores'].replace("$$", f"{j}")
+            item1 = self.driver.find_element(By.XPATH, valores).text
+            print(item1)
 
 
 
