78444d6892db162be94d99d87c96cb8066fc9722
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-05-16 16:36:45 -0300

    BancoInserir.py

--- BancoInserir.py
+++ BancoInserir.py
@@ -1,3 +1,14 @@
-from site import Abrir
 import mysql.connector
+cursor = 0
 
+try:
+    conexao = mysql.connector.connect(
+        host='localhost',
+        database='teste',
+        user='root',
+        password=' '
+    )
+except Exception as error:
+    print(error)
+else:
+    cursor = conexao.cursor()
